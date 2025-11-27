c4c45c3d0fce8ef49bbe57ed329828e8fc1357ef
Fawad Ghori <fawadghori@folio3.com>
2025-11-27 15:18:54 +0500

    Add project files.

---
+++ BlazorApp1/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public DateTime? BirthDate { get; set; }
+    }
+}
