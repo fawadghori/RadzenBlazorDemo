4eaf32efe2bfb0e012c234ef926ea60e340a08b5
Fawad Ghori <fawadghori@folio3.com>
2025-12-03 15:27:54 +0500

    Seperated the code file from razor file for create page

---
+++ BlazorApp1/Components/Pages/StudentPages/Create.razor.cs
@@ -0,0 +1,34 @@
+﻿using BlazorApp1.Models;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace BlazorApp1.Components.Pages.StudentPages
+{
+    public partial class Create
+    {
+        [SupplyParameterFromForm]
+        private Student Student { get; set; } = new();
+
+        private async Task AddStudent()
+        {
+            using var context = DbFactory.CreateDbContext();
+            context.Student.Add(Student);
+            await context.SaveChangesAsync();
+
+            NotificationService.Notify(new NotificationMessage
+            {
+                Severity = NotificationSeverity.Success,
+                Summary = "Success",
+                Detail = "Student created successfully",
+                Duration = 4000
+            });
+
+            NavigationManager.NavigateTo("/students");
+        }
+
+        private void BackToList()
+        {
+            NavigationManager.NavigateTo("/students");
+        }
+    }
+}
